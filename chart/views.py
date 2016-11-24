b333d3ed9d6bef7b07dde33eb905f39ec7e63564
Vins0n <vinsonring@gmail.com>
2016-11-24 14:31:04 +0800

    Beta X2

--- chart/views.py
+++ chart/views.py
@@ -2,22 +2,40 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import pandas as pd
 from ChartDjango.settings import CSV_PATH
-import os
+import os, re
+
+
+def get_file_list():
+    f_list = os.listdir(CSV_PATH)
+    f_list_no_postfix = []
+    for f_item in f_list:
+        if os.path.splitext(f_item)[1].lower() == '.csv':
+            f_list_no_postfix.append(os.path.splitext(f_item)[0])
+
+    return f_list_no_postfix
+
+
+def get_file_name(request):
+    request_url = request.path
+    rex = re.compile(r'[\w]*.[\w]*$')
+    return rex.findall(request_url)[0]
+
+
+def get_file_name_no_postfix(file_name):
+    rex = re.compile(r'[\w]*')
+    return rex.findall(file_name)[0]
 
 
 def index(request):
-    CSV_FILES = os.listdir(CSV_PATH)
-    return render(request, 'csv_OHLC.html', {'csv_files': CSV_FILES})
+    return render(request, 'base.html', {'file_list': get_file_list()})
 
 
 def show_csv_item(request):
-    CSV_FILES = os.listdir(CSV_PATH)
-
-    csv_file_name = request.path[5:]
-
-    if csv_file_name:
+    file_name = get_file_name(request)
+    file_name_no_postfix = get_file_name_no_postfix(file_name)
+    if file_name:
         try:
-            df = pd.read_csv(CSV_PATH + csv_file_name)
+            df = pd.read_csv(CSV_PATH + file_name)
         except IOError:
             return HttpResponse("The File does not exist!")
     else:
@@ -25,15 +43,11 @@ def show_csv_item(request):
 
     if 'highp' and 'lowp' in df.columns.tolist():
         df_json = df.to_json(orient="values")
-        return render(request, 'csv_OHLC.html', {'df_json': df_json, 'csv_files': CSV_FILES})
+        return render(request, 'csv_OHLC.html', {'df_json': df_json, 'file_name_no_postfix':file_name_no_postfix, 'file_list': get_file_list()})
 
     elif 'dates' and 'closep' in df.columns.tolist():
         df_json = df.to_json(orient="values")
-        return render(request, 'csv_line.html', {'df_json': df_json, 'csv_files': CSV_FILES})
+        return render(request, 'csv_line.html', {'df_json': df_json, 'file_name_no_postfix':file_name_no_postfix, 'file_list': get_file_list()})
 
     else:
         return HttpResponse("Wrong Data")
-
-
-# df_json = df.to_json(orient="values")
-# return HttpResponse(df_json)
